1f4ffce266d0eaa47cafead3cbd490ca1376d158
StrukovOI <strukov.oi@phystech.edu>
2025-09-16 14:01:17 +0300

    .

--- seminar2_type/02.cpp
+++ seminar2_type/02.cpp
@@ -4,9 +4,6 @@ unsigned long long int razm(int n, int k)
     unsigned long long int fn = 1;
     for (int i = n - k + 1; i <= n; ++i)
         fn *= i;
-    // for (int i = 1; i <= (n-k); ++i)
-    //     fk *= i;
-    // res=fn/fk;
 
     return fn;
 }

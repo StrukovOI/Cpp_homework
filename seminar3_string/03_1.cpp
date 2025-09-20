29a050757c931aed476b28e05bc51e10b6ed6321
StrukovOI <strukov.oi@phystech.edu>
2025-09-20 17:13:57 +0300

    .

--- seminar3_string/03_1.cpp
+++ seminar3_string/03_1.cpp
@@ -4,7 +4,7 @@ int main()
 {
     char a;
     scanf("%c", &a);
-    if (a >= 'A' && a <= 'Z' || a >= 'a' && a <= 'z')
+    if (a >= '65' && a <= '90' || a >= '97' && a <= '122')
         printf("Letter");
     else if (a >= 48 && a <= 57)
         printf("Digit");

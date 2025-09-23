f610f0a7fb1c6694e37ba171cddfa2c7ab15ad1c
StrukovOI <strukov.oi@phystech.edu>
2025-09-23 14:19:09 +0300

    .

---
+++ seminar3_string/07.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+void encrypt(char* str, int k)
+{
+	for (size_t i = 0; i < strlen(str); ++i)
+	{
+
+    	if (str[i] <= 'Z' && str[i] > 'A')
+		{
+			str[i] += k;
+			if (str[i] > 'Z')
+			{
+				str[i] -= 26;
+			}			
+		}
+
+		else if (str[i] >= 'a')
+		{
+			str[i] += k;
+			if (str[i] > 'z')
+			{
+				str[i] -= 26;
+			}
+		}
+	}
+}
+
+int main()
+{
+    int k;
+    char c[100];
+	scanf("%i", &k);
+
+	getchar();
+	fgets(c, sizeof(c), stdin);
+    c[strcspn(c, "\n")] = '\0';
+
+    encrypt(c, k);
+    printf("%s", c);
+}

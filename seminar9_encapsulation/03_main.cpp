696a2924322d12e7dd749ee92c666be49c15e85f
StrukovOI <strukov.oi@phystech.edu>
2025-11-15 14:23:12 +0300

    .

---
+++ seminar9_encapsulation/03_main.cpp
@@ -0,0 +1,11 @@
+#include "03_number.hpp"
+
+int main() 
+{
+    Number x = 12345678;
+    Number y = 0;
+    std::cout << x << " " << y << std::endl;
+
+	std::cout << fibonacci(1000) << std::endl << factorial(1000);
+
+}

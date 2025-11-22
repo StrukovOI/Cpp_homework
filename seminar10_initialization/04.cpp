02ce2a4c547c5caab5b3cbc60a5af4ebdfd394ac
StrukovOI <strukov.oi@phystech.edu>
2025-11-22 03:12:37 +0300

    .

---
+++ seminar10_initialization/04.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Cat 
+{
+private:
+    int& miceCount;
+public:
+    Cat(int& count) : miceCount(count) {}
+
+    void catchMice(int count) 
+    {
+        miceCount += count;
+    }
+};
+
+int main() 
+{
+    int miceCaughtA = 0;
+    int miceCaughtB = 0;
+
+    Cat alice(miceCaughtA), alex(miceCaughtA), anna(miceCaughtA);
+    Cat bob(miceCaughtB), bella(miceCaughtB);
+
+    alice.catchMice(2);
+    alex.catchMice(1);
+    bella.catchMice(4);
+    bob.catchMice(2);
+    anna.catchMice(1);
+    bella.catchMice(1);
+    alex.catchMice(4);
+    bella.catchMice(5);
+    alice.catchMice(2);
+
+    std::cout << miceCaughtA << std::endl;
+    std::cout << miceCaughtB << std::endl;
+}
